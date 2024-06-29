a385f792bcb3a1d9a2409c5bbd8280f66347e3fa
MoFawarah <mfawarah1@gmail.com>
2024-06-29 19:21:37 +0300

    Updated 29 June

---
+++ 27.6.2024/27.6.2024/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._6._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //problem solving: https://www.w3resource.com/csharp-exercises/string/index.php
+
+            //PS 01: I want you to display whatever the user inputs alone. If they enter a word
+            //then show it alone, and if they enter a phrase, show it alone.
+
+
+            //Console.Write("Plz enter a word or phrase: ");
+            //string userInput = Console.ReadLine();
+
+            //if (string.IsNullOrEmpty(userInput)) {
+            //    Console.WriteLine("You didn't enter anything!");
+            //} else {
+
+            //    string[] userPhrases = userInput.Split(); //splits at the space.
+
+
+            //    if (userPhrases.Length == 1) {
+
+            //        Console.WriteLine($"The word you entered is: {userPhrases[0]}");
+
+            //    } else {
+
+            //        Console.Write($"The phrase you entered is: ");
+            //        for (int i = 0; i < userPhrases.Length; i++) {
+            //            Console.Write(userPhrases[i] + " ");
+
+            //        }
+
+            //        Console.WriteLine();
+
+            //    }
+            //}
+
+
+            //PS 02: Find the string of a string using the library (length)
+
+            //Console.WriteLine("Enter any word or phrase: ");
+            //string phraseEntered = Console.ReadLine();
+
+            //int phraseLength = phraseEntered.Length;
+            //Console.WriteLine("The length of the phrase/word you entered is: " + phraseLength);
+
+
+            //PS 02: Find the string of a string ////wihtout//// using the library (length)
+
+            //Console.WriteLine("Plz enter a word or a phrase: ");
+
+            //string str;
+            //int l = 0;  
+
+            //str = Console.ReadLine();
+
+            //foreach(char c in str)
+            //{
+            //    l++;
+            //}
+            //Console.WriteLine("The length of the phrase/word you entered is: " + l);
+
+
+
+
+            ////////////////// NEW PROBLEM SOLVING //////////////////////
+            //PS 03: Write a C# Sharp program to separate individual characters from a phrase.
+
+            //Console.WriteLine("Plz enter a word or a phrase separated by a comma: ");
+
+            //string str = Console.ReadLine();
+
+            //string [] stringSeparated = str.Split(new string[] {","}, StringSplitOptions.RemoveEmptyEntries);
+
+            //foreach (string s in stringSeparated) {
+            //    Console.WriteLine(s);
+            //}
+
+
+            //PS 04: Write a C# Sharp program to separate individual characters from a word.
+
+            //Console.WriteLine("Plz enter a word: ");
+
+            //string str = Console.ReadLine();
+
+            //foreach (char ch in str) {
+            //    Console.Write(ch + " ");
+
+            //}
+
+            //PS 05: Write a program in C# Sharp to print individual characters of the string in ///reverse order///.
+
+
+
+            //Console.WriteLine("Plz enter a word: ");
+
+            //string str = Console.ReadLine();
+
+            //int l;
+
+            //for (l = str.Length - 1; l>=0; l--)
+            //{
+            //    Console.Write(str[l] + " ");
+
+            //}
+
+            //Console.WriteLine();
+
+
+            //PS 06: Write a program in C# Sharp to count the total number of words in a string.
+
+
+            //Console.WriteLine("Plz enter a word or phrase : ");
+
+            //string str = Console.ReadLine();
+
+            //string [] strSeperated = str.Split(' ');
+
+            //int l = strSeperated.Length;
+            //Console.WriteLine("The number of words you entered is: {0}", l);
+
+
+            //PS 07: Write a program in C# Sharp to compare two strings without using a string library functions.
+
+            ///////////////come back to it later //////////////
+
+            Console.WriteLine("Plz enter a word or phrase : ");
+
+            string str01 = Console.ReadLine();
+            string[] str01Splited = str01.Split();
+
+            int str01Length = str01Splited.Length;
+
+            Console.WriteLine("Plz enter a word or phrase : ");
+
+            string str02 = Console.ReadLine();
+            string[] str02Splited = str02.Split();
+
+            int str02Length = str02Splited.Length;
+
+            if (str01Length == str02Length)
+            {
+                Console.WriteLine("The two strings are equal in length");
+
+                for (int i = 0; i < str01Length; i++)
+                {
+                    if (str02Splited[i] != str01Splited[i])
+                    {
+                        Console.WriteLine("The two strings are equal in words too");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("The two strings are NOT equal in length");
+                    }
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("The two strings are NOT equal in length");
+
+                for (int i = 0; i < str01Length; i++)
+                {
+                    if (str02Splited[i] == str01Splited[i])
+                    {
+                        Console.WriteLine("The two strings are equal in words too");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("The two strings are NOT equal in length");
+                    }
+                }
+
+
+
+
+
+
+            }
+        }
+    }
+}
---
+++ 29.6.2024/29.6.2024/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29._6._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //task 01: Correcting Syntax Errors:
+
+            int[] ARR = { 1, 7, 9, 45 }; // old >>> string [ ] ARR= [ 1,7  9  45, ]
+
+            int[] ARR2 = { 20, 50 };
+
+            int[] newArray = ARR.Concat(ARR2).ToArray();
+
+            string arrayString = "{" + string.Join(", ", newArray) + "}";
+
+            Console.WriteLine(arrayString);
+
+
+            //////Another Syntax Error/////////
+
+            string [] arr2 = { "Str", "alex", "moh" }; // old >>> int arr2=["Str" "alex","moh"
+
+            string array2Joined = "[" + string.Join(", ", arr2) + "]";
+            Console.WriteLine(array2Joined);
+
+
+            //////Another Syntax Error/////////
+            string[] arr3 = { "the", "fox", "over", "lazy", "dog" };
+
+            string arr3Joined = "[" + string.Join(", ", arr3) + "]";
+            Console.WriteLine(arr3Joined);
+
+
+            //task 02: Finding the index of a certain item in an array
+
+            string[] fruits = { "Tomato", "Banana", "Watermelon", "Burries", "Pears" };
+
+            int tomatoIndex = Array.IndexOf(fruits, "Tomato");
+            int bananaIndex = Array.IndexOf(fruits, "Banana");
+            int burriesIndex = Array.IndexOf(fruits, "Burries");
+            Console.WriteLine("The index of Tomato is: " + tomatoIndex);
+            Console.WriteLine("The index of Banana is: " + bananaIndex);
+            Console.WriteLine("The index of Burries is: " + burriesIndex);
+
+
+            //task 03: Creating arrays that represent personal stuff
+            string[] favFood = { "Mansaf", "Shawarma", "Fish", "Hummus", "Salad" };
+            string[] favSports = { "Soccer", "Basketball", "Taekwondo", "Rope Jumping", "Tennis" };
+            string[] favMovies = { "Last Christmas", "Luka", "The Pursuit of Happyness", "Friends" };
+
+            Console.WriteLine("My favorite food is: ");
+            foreach (var fav in favFood)
+            {
+                Console.Write(fav + " ");
+            }
+            Console.WriteLine(); 
+
+            Console.WriteLine("My favorite sports are: ");
+            foreach (var fav in favSports)
+            {
+                Console.Write(fav + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("My favorite movies are: ");
+            foreach (var fav in favMovies)
+            {
+                if (fav == "Friends")
+                {
+                    Console.Write(fav + ".");
+                }
+
+                else {
+
+                    Console.Write(fav + ", ");
+                }
+               
+            }
+            Console.WriteLine();
+
+
+            //task 04:
+
+            //Console.Write("Plz input three numbers seperated by a comma: ");
+
+            //string [] userInput = Console.ReadLine().Split(',');
+
+            //int[] userInputArray = Array.ConvertAll(userInput, int.Parse);
+
+            //double sum = 0;
+
+            //foreach (int y in userInputArray) { 
+            //  sum += y;
+            //}
+
+            //Console.WriteLine("The total sum of your inputted numbers is: " + sum);
+
+
+            //another method:
+
+
+            //Console.Write("Plz input three numbers seperated by a comma: ");
+
+            //int [] userInputArray = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+
+            //double sum = 0;
+
+            //foreach (int y in userInputArray)
+            //{
+            //    sum += y;
+            //}
+
+            //Console.WriteLine("The total sum of your inputted numbers is: " + sum);
+
+
+            //task 05: Write a program in C# to display the n terms of odd number and their sum from [1- 100 ]
+
+            //int sum001 = 0;
+
+            //for (int i = 0; i <= 100; i++) { 
+            // if (i%2 != 0)
+            //    {
+            //        Console.Write(i + " ");
+            //        sum001 += i;
+
+            //    }
+            //}
+
+            //Console.WriteLine("\nThe Sum of odd Numbers is: " + sum001);
+
+
+            //Task 06: 
+
+            //int numberOfRows = 4;
+
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+
+        
+
+            
+
+           
+            int currentNumber = 1;
+
+            for (int i = 1; i <= 4; i++)
+            {
+                // for spaces
+                for (int j = 4; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                // for numbers
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(currentNumber + " ");
+                    currentNumber++;
+                }
+
+                // Move to the next line
+                Console.WriteLine();
+            }
+        }
+    }
+}
